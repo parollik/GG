598e1d7237b14566913ffd4f27ae5e24a1471de0
parollik <99071669+parollik@users.noreply.github.com>
2022-02-05 11:26:07 +0200

    Add files via upload

---
+++ Кира/script.js
@@ -0,0 +1,26 @@
+
+function funk1(){
+  alert('функция активна');
+}
+
+function summ(a,b,c){
+
+  return a+b+c;
+}
+/*
+var a = prompt('Введте z');
+
+if(a == 'z'){
+   funk1();
+}
+*/
+    // var sum = summ(4,8,1);
+/*
+var a = parseInt(prompt('Введите число'));
+var b = parseInt(prompt('Введите число'));
+var x = parseInt(prompt('Введите число'));
+
+alert('Сумма = ' + summ(a,b,x));
+
+alert('Конец кода');
+*/
